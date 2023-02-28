945d7ea432528caddb2b37a8229dfff51d915e3a
Marjorie Mendoza <marjoriemendoza04@hotmail.com>
2023-02-28 10:59:56 -0600

    agregue las clases

---
+++ SysControlVivero.EntidadesDeNegocio/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysControlVivero.EntidadesDeNegocio
+{
+    internal class Usuario
+    {
+    }
+}
