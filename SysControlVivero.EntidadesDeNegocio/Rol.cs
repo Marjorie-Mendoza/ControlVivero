7bb476ab667086cffe8d6472ad12734e1f72c58e
Marjorie Mendoza <marjoriemendoza04@hotmail.com>
2023-02-28 22:02:03 -0600

    hice un cambio en la clase rol

--- SysControlVivero.EntidadesDeNegocio/Rol.cs
+++ SysControlVivero.EntidadesDeNegocio/Rol.cs
@@ -12,6 +12,6 @@ namespace SysControlVivero.EntidadesDeNegocio
         public string Nombre { get; set; }
         public int Nivel { get; set; }
 
-           public List<Rol> rols { get; set; }
+           public List<Usuario> usuarios { get; set; }
     }
 }
