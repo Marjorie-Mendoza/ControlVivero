2f96bc0427fe4c53a6e040d5e81fbd6f987b8398
Elsy Pineda <jaz_elsy@hotmail.com>
2023-03-13 11:06:11 -0600

    Anotaciones de datos en la tabla rol

--- SysControlVivero.EntidadesDeNegocio/Rol.cs
+++ SysControlVivero.EntidadesDeNegocio/Rol.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SysControlVivero.EntidadesDeNegocio
 {
     public class Rol
-    {
-        public int Cod { get; set; }
+    {        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(20,ErrorMessage = "Maximo 50 caracteres")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Especificar el número de nivel")]
+        [ StringLength(1,ErrorMessage ="numero maximo de caracter 1",MinimumLength =1)]
         public int Nivel { get; set; }
 
            public List<Usuario> usuarios { get; set; }
