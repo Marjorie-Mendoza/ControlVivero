cc3e7cbed00f03298d9102ce848b08e1aa9abf9e
Elsy Pineda <jaz_elsy@hotmail.com>
2023-03-13 11:09:24 -0600

    Hola

--- SysControlVivero.EntidadesDeNegocio/Rol.cs
+++ SysControlVivero.EntidadesDeNegocio/Rol.cs
@@ -19,7 +19,7 @@ namespace SysControlVivero.EntidadesDeNegocio
         [Required(ErrorMessage = "Especificar el número de nivel")]
         [ StringLength(1,ErrorMessage ="numero maximo de caracter 1",MinimumLength =1)]
         public int Nivel { get; set; }
-        //hhhh
+        //Hola Soy David
            public List<Usuario> usuarios { get; set; }
     }
 }
