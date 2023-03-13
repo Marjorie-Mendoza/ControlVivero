e367a9c751d97808d4c1b255024ff217966db6ae
Marjorie Mendoza <marjoriemendoza04@hotmail.com>
2023-03-13 11:03:09 -0600

    le agregamos anotaciones

--- SysControlVivero.EntidadesDeNegocio/Usuario.cs
+++ SysControlVivero.EntidadesDeNegocio/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,25 @@ namespace SysControlVivero.EntidadesDeNegocio
 {
     public class Usuario
     {
-        public int Cod { get; set; }
-        public string Nombre { get; set; }
-        private string Clave { get; set; }
+        
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage ="Rol es obligatorio")]
+        public int IdRol { get; set; }
+        [ForeignKey("Empleado")]
+        [Required(ErrorMessage = "Empleado es obligatorio")]
+        public int IdEmpleado { get; set; }
 
-           public Rol rol { get; set; }
-        public Empleado empleados { get; set; }
+        [Required(ErrorMessage ="Nombre es hobligatorio")]
+        [StringLength(50,ErrorMessage ="Maximo 50 caracteres")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage ="este campo es obligatorio")]
+        [StringLength(50,ErrorMessage ="maximo 8 caracteres",MinimumLength =8)]
+        private string? Clave { get; set; }
+  
+
+        public Rol? rol { get; set; }
+        public Empleado? empleados { get; set; }
     }
 }
