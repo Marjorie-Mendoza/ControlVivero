53915908e9c19810665c5ee03c23bbcd9494eb6d
Marjorie Mendoza <marjoriemendoza04@hotmail.com>
2023-03-15 20:34:16 -0600

    HOLKKLKLKL

--- SysControlVivero.AccesoADatos/ProductoDAL.cs
+++ SysControlVivero.AccesoADatos/ProductoDAL.cs
@@ -46,7 +46,7 @@ namespace SysControlVivero.AccesoADatos
             }
 
         }
-        //BTS FOREVER WE ARE YOUNG 
+        //FOREVER WE ARE YOUNG 
         public int EliminarProducto(List<Producto> ListaProducto, int id)
         {
             if (id != 0)
